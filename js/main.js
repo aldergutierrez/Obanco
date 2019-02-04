ef542be00b5a9fd71840b625aa53b28963ff762a
aldergutierrez <aldergutierrez@outlook.com>
2019-02-04 17:46:53 -0600

    ya casi queda
    ya merito

--- js/main.js
+++ js/main.js
@@ -12,6 +12,24 @@ Enjoy responsibly!
 
 */ 
 
+
+//set button id on click to hide first modal
+$("#signin").on( "click", function() {
+        $('#myModal1').modal('hide');  
+});
+//trigger next modal
+$("#signin").on( "click", function() {
+        $('#myModal2').modal('show');  
+});
+//set button id on click to hide first modal
+$("#escanear").on( "click", function() {
+        $('#myModal2').modal('hide');  
+});
+//trigger next modal
+$("#escanear").on( "click", function() {
+        $('#myModal3').modal('show');  
+});
+
 $(document).ready(function() {
 	
 	setTimeout(function(){
